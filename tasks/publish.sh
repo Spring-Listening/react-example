28c4d7c55cc9864496e66d6eb7694bb43622d3f9
spring_zou <326359613@qq.com>
2018-10-09 09:36:16 +0800

    事实上

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -18,7 +18,6 @@ cd "$(dirname "$0")"
 # We assume that all the commands in the pipeline set their return code
 # properly and that we do not need to validate that the output is correct
 set -e
-
 # Echo every command being executed
 set -x
 
@@ -29,7 +28,6 @@ root_path=$PWD
 if [ -n "$(git status --porcelain)" ]; then
   echo "Your git status is not clean. Aborting.";
   exit 1;
-fi
 
 # Compile
 cd packages/react-error-overlay/
